33b963e417ffcadf497b76281395f96fd7846ed8
Jack Pena <penajackc@gmail.com>
2022-03-06 11:14:54 -0700

    commit tree a8f874c52e7d3a31e2740930c9317eb1e8c6a035

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,4 +23,24 @@ class ApplicationController < Sinatra::Base
     baked_good.to_json
   end
 
+  post '/baked_goods' do
+    newbg = BakedGood.create(
+      name: params[:name],
+      price: params[:price],
+      bakery_id: params[:bakery_id]
+    )
+    newbg.to_json
+  end
+
+  patch '/bakeries/:id' do
+    bg = Bakery.find(params[:id])
+    bg.update(name: params[:name])
+    bg.to_json
+  end
+
+  delete '/baked_goods/:id' do
+    bg = BakedGood.find(params[:id])
+    bg.destroy
+    bg.to_json
+  end
 end
